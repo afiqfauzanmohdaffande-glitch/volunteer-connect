a85222ab9efcacd74a44899f409099970583c5cb
afiqfauzanmohdaffande-glitch <afiqfauzanmohdaffande@gmail.com>
2026-05-25 16:08:15 +0800

    Initial commit: Setup project configuration and core files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import VolunteerApp from "./components/VolunteerApp";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <VolunteerApp />
+    </div>
+  );
+}
